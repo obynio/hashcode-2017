0571adf689f621ba8513b28508f29dde9effc657
Eloi Charpentier <eloi.charpentier@epita.fr>
2017-02-24 04:24:10 +0100

    Fixed bugs

--- buffer.hh
+++ buffer.hh
@@ -1,16 +1,19 @@
 #pragma once
 
-
+#include "data.hh"
 #include <vector>
 #include <map>
 
 class Buffer
 {
   public:
+    Buffer()
+    {
+      free_size = Data::capacity_cache;
+    }
     std::vector<int> videos;
     std::map<int, int> gain_per_video;
     std::map<int, int> inv_gain;
 
     int free_size;
-    static int total_size;
 };
--- state.cc
+++ state.cc
@@ -7,36 +7,37 @@
 State::State()
 {
   buffers.resize(Data::nb_cache);
-  for (auto it : buffers)
+  int freesize = Data::capacity_cache;
+  for (auto& it : buffers)
   {
-    it.free_size = Data::capacity_cache;
+    it.free_size = freesize;
   }
 }
 
 
 void State::initial_solution()
 {
-  for (auto endpoint : Data::endpoints)
+  for (auto& endpoint : Data::endpoints)
   {
-    for (auto buff : endpoint.cost_per_buffer)
+    for (auto& buff : endpoint.cost_per_buffer)
     {
       int diff = endpoint.cost - buff.second;
       if (diff < 0)
         break;
-      for (auto vid : endpoint.request_per_video)
+      for (auto& vid : endpoint.request_per_video)
       {
         buffers[buff.first].gain_per_video[vid.first] += diff * vid.second;
       }
     }
   }
 
-  for (auto buff : buffers)
+  for (auto& buff : buffers)
   {
-    for (auto it : buff.gain_per_video)
+    for (auto& it : buff.gain_per_video)
     {
       buff.inv_gain[it.second] = it.first;
     }
-    for (auto it : buff.inv_gain)
+    for (auto& it : buff.inv_gain)
     {
       if (buff.free_size < Data::video_size[it.second])
         break;
@@ -56,7 +57,7 @@ void State::export_file()
     myfile << i;
     for (int v = 0; v < ((int)buffers[i].videos.size()) - 1; v++)
     {
-      myfile << buffers[i].videos[v] << " ";
+      myfile <<  " " << buffers[i].videos[v];
     }
     myfile << "\n";
   }
